98a21bca9dffa6e358bd6928133df12290d7fe55
alishbanazar <alishbanazarmuhammadabbasi@gmail.com>
2024-04-27 11:10:22 +0500

    done variable for numbers

---
+++ app.js
@@ -0,0 +1,20 @@
+// question#1
+var age = "20"
+alert("I am " + age + " years old");
+
+// question#2
+var webPage = "You have visited this sides for 14 times "
+alert(webPage)
+
+
+// question#3
+var birthmonth = "My birth month is september";
+document.write(birthmonth + "<br>" + "Data type of my declared variable is number" + "<br>"  )
+
+// question#4
+var visitorName = "<b> Charles </b>";
+var productName = "ordered 5 pizza from";
+var orderPlace = "<b> Pizza Max </b>";
+document.write(visitorName + productName + orderPlace)
+    
+    //    completed
